26cf87c36781c87ad4570a337d13cef4fbd551c3
DanielD1234 <danieldinca97@yahoo.com>
2019-11-26 20:48:39 +0200

    Add persistent sql and admin app
    Signed-off-by: DanielD1234 <danieldinca97@yahoo.com>

--- server_img/src/main.py
+++ server_img/src/main.py
@@ -1,26 +1,101 @@
 import pymysql
 import time
+import sys
 
-# wait until sql database fully loaded
-time.sleep(15)
+db = None
 
-# Open database connection
-db = pymysql.connect(host = 'mysql-dev', user = 'root', password = 'password', db = 'flights')
+def connect_to_database():
+    global db
 
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
+    # Open database connection
+    db = pymysql.connect(host = "mysql-dev", user = "root", password = "password", db = "flights")
 
-# execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
 
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
-print ("Database version : %s " % data)
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT VERSION()")
 
-cursor.execute("select * from flights_table")
-rows = cursor.fetchall()
-for row in rows:
-    print(row[0],row[1],row[2])
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print ("Database version : %s " % data)
 
-# disconnect from server
-db.close()
+def print_possible_commands():
+    print("help - print this list")
+    print("add flight_id, flight_origin, flight_departure, dep_day, dep_hour, duration, seats_number")
+    print("cancel flight_id")
+    print("print - prints current flights")
+    print("exit - exits administration app")
+
+def print_flights():
+    cursor = db.cursor()
+    cursor.execute("select * from flights_table")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+
+def add_flight(cmd):
+    flight_columns = cmd.split(' ')
+    values_to_add = "({}, \"{}\", \"{}\", {}, {}, {}, {})".format(flight_columns[1],
+                                                                  flight_columns[2],
+                                                                  flight_columns[3],
+                                                                  flight_columns[4],
+                                                                  flight_columns[5],
+                                                                  flight_columns[6],
+                                                                  flight_columns[7])
+    cursor = db.cursor()
+    try:
+        cursor.execute("insert into flights_table values " + values_to_add)
+    except pymysql.err.MySQLError as e:
+        print(e)
+
+    db.commit()
+    cursor.close()
+
+def cancel_flight(cmd):
+    delete_flight_id = cmd.split(' ')[1]
+
+    cursor = db.cursor()
+    try:
+        cursor.execute("delete from flights_table where id = " + delete_flight_id)
+    except pymysql.err.MySQLError as e:
+        print(e)
+
+    db.commit()
+    cursor.close()
+
+
+def get_commands():
+    print_possible_commands()
+
+    while True:
+        line = input("$- ")
+
+        if line == "help":
+            print_possible_commands()
+        elif line == "exit":
+            break
+        elif line == "print":
+            print_flights()
+        elif line.split(" ")[0] == "add":
+            add_flight(line)
+        elif line.split(" ")[0] == "cancel":
+            cancel_flight(line)
+
+def main():
+    # wait until sql database fully loaded
+    time.sleep(15)
+
+    connect_to_database()
+
+    get_commands()
+
+    # disconnect from server
+    db.commit()
+    db.close()
+
+if __name__ == "__main__":
+    main()
--- sql_setup/create_table.sql
+++ sql_setup/create_table.sql
@@ -1,11 +1,12 @@
 create table flights_table(
     id              int not null,
-    depart          varchar(50),
+    origin          varchar(50),
     destination     varchar(50),
     depart_date     int,
+    depart_hour     int,
     duration        int,
     number_seats    int,
     primary key(id)
 );
 
-INSERT INTO flights_table VALUES (1, 'Bucuresti', 'Timisoara', 2, 2, 50);
+INSERT INTO flights_table VALUES (1, 'Bucuresti', 'Timisoara', 2, 23, 1, 50);
